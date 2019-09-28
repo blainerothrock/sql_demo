f79381d17502d5edfb02267f0e987ec29449253e
Blaine Rothrock <blainerothrock2020@u.northwestern.edu>
2019-09-28 12:03:39 -0500

    init scripts

---
+++ scripts/02_where.sql
@@ -0,0 +1,11 @@
+-- SELECT *
+-- FROM data_officer
+-- WHERE birth_year = 1990;
+--
+-- SELECT *
+-- FROM data_officer
+-- WHERE resignation_date = '2016-07-31';
+--
+SELECT *
+FROM data_officer
+WHERE civilian_allegation_percentile > 99.9
