ecbede02192662655bb40fc141af6ee6481a5f48
Blaine Rothrock <blainerothrock2020@u.northwestern.edu>
2019-10-03 10:14:06 -0500

    index

--- scripts/27_index.sql
+++ scripts/27_index.sql
@@ -1,1 +1,12 @@
-CREATE INDEX idx_
+-- SELECT *
+-- FROM data_allegation
+-- WHERE incident_date = '2016-07-4';
+
+CREATE INDEX idx_incident_date
+ON data_allegation (incident_date)
+
+-- SELECT *
+-- FROM data_allegation
+--  WHERE incident_date = '2017-07-04'
+
+-- DROP INDEX idx_incident_date;
